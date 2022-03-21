983a0abacbbe989ced15d61c1491df27cd34dafb
xkritti <krittamet0404@gmail.com>
2022-03-21 22:56:26 +0700

    add axios for FetchData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 
 
 
 function App() {
+  const [Data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get("https://tcas-assets.skooldio.com/tmp/mock_tcaster_api.json")
+    setData(res.data)
+    console.log(res.data)
+
+  };
+  useEffect(() => {
+    fetchData()
+  }, []);
   return (
     <div className="App">
+      {Data.map((item, index) => {
+        return (
+          <Card key={index} Data={item} />
+        )
+      })}
       <Card />
     </div>
   );
--- src/components/BodyCrad/ContentFooter/ContentFooter.js
+++ src/components/BodyCrad/ContentFooter/ContentFooter.js
@@ -5,7 +5,7 @@ const ContentFooter = () => {
         <div>
             <div className="line"/>
             <div style={{display:'flex',justifyContent:'flex-start'}}>
-                <h3 style={{paddingRight:"10px"}}>>></h3>
+                <h3 style={{paddingRight:"10px"}}>{'>>'}</h3>
             <h3>ดูสัดส่วนคะแนน</h3>
             </div>
             
